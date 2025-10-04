ef20867668aaf1e5d06c531c98a3cfec1039f485
Augsorn Chanklad <uppban5@gmail.com>
2025-10-04 12:38:09 +0700

    test: add mock booking list

--- src/features/bookings/components/booking-list/booking-list.stories.tsx
+++ src/features/bookings/components/booking-list/booking-list.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 
 import { BookingList } from "./booking-list";
+import { http, HttpResponse } from "msw";
+import { paths } from "@/config/paths";
+import { mockBookingData } from "../../__test__/mock-booking-data";
 
 const meta: Meta<typeof BookingList> = {
   title: "Features/Bookings/BookingList",
@@ -8,6 +11,13 @@ const meta: Meta<typeof BookingList> = {
   tags: ["autodocs"],
   parameters: {
     layout: "fullscreen",
+    msw: {
+      handlers: [
+        http.get(paths.api.bookings.list(), () => {
+          return HttpResponse.json(mockBookingData);
+        }),
+      ],
+    },
   },
   args: {},
 };
