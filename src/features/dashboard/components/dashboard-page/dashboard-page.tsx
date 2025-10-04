34ead8c9e4e0627531a8d1be37d8c41eb627e260
Augsorn Chanklad <uppban5@gmail.com>
2025-10-04 14:12:04 +0700

    feat: add padding dashboard

--- src/features/dashboard/components/dashboard-page/dashboard-page.tsx
+++ src/features/dashboard/components/dashboard-page/dashboard-page.tsx
@@ -7,7 +7,7 @@ import { BookingTrendsCard } from "../booking-trends-card";
 
 export const DashboardPage = () => {
   return (
-    <Column>
+    <div className="p-4 md:p-6 lg:p-8 flex- flex-col gap-4 md:gap-6 lg:gap-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <SummaryCard
           label="Total booking"
@@ -43,6 +43,6 @@ export const DashboardPage = () => {
         <RoomAvailabilityCard />
         <BookingTrendsCard />
       </div>
-    </Column>
+    </div>
   );
 };
